dbd8886d8699bba982d658634ab0a398143089ba
FloodGuard Dev <dev@floodguard.lk>
2026-09-22 01:36:14 +0530

    add sriLankaLocations data for geographical reference

---
+++ frontend/src/data/sriLankaLocations.js
@@ -0,0 +1,13 @@
+const sriLankaLocations = {
+  Western: ['Colombo', 'Gampaha', 'Kalutara'],
+  Central: ['Kandy', 'Matale', 'Nuwara Eliya'],
+  Southern: ['Galle', 'Matara', 'Hambantota'],
+  Northern: ['Jaffna', 'Kilinochchi', 'Mannar', 'Vavuniya', 'Mullaitivu'],
+  Eastern: ['Trincomalee', 'Batticaloa', 'Ampara'],
+  'North Western': ['Kurunegala', 'Puttalam'],
+  'North Central': ['Anuradhapura', 'Polonnaruwa'],
+  Uva: ['Badulla', 'Monaragala'],
+  Sabaragamuwa: ['Ratnapura', 'Kegalle'],
+};
+
+export default sriLankaLocations;
